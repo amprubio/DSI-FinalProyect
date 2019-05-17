df2321147a36926ec5a76669bc1eec98f7fd335f
Amph <amprubio@ucm.es>
2019-05-17 21:47:01 +0100

    barra de vida hecha

--- DSI-FinalProyect/MainPage.xaml.cs
+++ DSI-FinalProyect/MainPage.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using System.Windows;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
 
@@ -38,7 +25,7 @@ namespace DSi_FinalProyect
 
         private void AppBarButton_Click(object sender, RoutedEventArgs e)
         {
-           
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
